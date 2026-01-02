eacecf4d9718dcaf416a0009de7bd8c95713e8d1
yichenn98 <59024224+yichenn98@users.noreply.github.com>
2026-01-02 23:15:23 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/record-form-ai/',   // ⭐⭐⭐ 加這一行
+      base: '/B/',   // ⭐⭐⭐ 加這一行
       server: {
         port: 3000,
         host: '0.0.0.0',
